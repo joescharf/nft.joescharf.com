22422a633097c9f4ca8167c30a4c19d4a0764b3d
Joe Scharf <joe@joescharf.com>
2022-02-07 14:28:50 -0700

    Updated footer and link styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,25 +9,38 @@ export default function Footer(): React.ReactElement {
   ) as React.ContextType<NetworkContext>
 
   return (
-    <footer className="border-t">
-      <p>
+    <footer className="mt-5 border-t">
+      <p className="mt-2">
         Scharfnado NFT Marketplace - Based on the excellent{' '}
-        <a href="https://www.youtube.com/watch?v=GKJBEEXUha0">
-          NFT Tutorial by Nader Dabit
+        <a href="https://www.youtube.com/watch?v=GKJBEEXUha0" target={'_blank'}>
+          NFT Marketplace Tutorial by Nader Dabit
         </a>
       </p>
-      <p>(C)networkInfo.default: {networkInfo?.default}</p>
-      <p>(C)networkInfo.network: {networkInfo?.network}</p>
-      <p>
-        (C)networkInfo.nftMarketABI.address:{' '}
-        {networkInfo?.nftMarketABI?.address}
+      <p className="mt-2">
+        <a
+          href="https://github.com/joescharf/nft.joescharf.com"
+          target={'_blank'}
+        >
+          Github repo
+        </a>{' '}
+        | Contact me on twitter:{' '}
+        <a href="https://twitter.com/joescharf" target={'_blank'}>
+          @joescharf
+        </a>
       </p>
-      <p>(C)networkInfo.nftABI.address: {networkInfo?.nftABI?.address}</p>
-      <p>
-        (C)networkInfo.snadoFaucetABI.address:{' '}
-        {networkInfo?.snadoFaucetABI?.address}
-      </p>
-      <p>Environment: {environment}</p>
+      <div className="mt-2 text-gray-400 border-t border-blue-300 ">
+        <p>networkInfo.default: {networkInfo?.default}</p>
+        <p>networkInfo.network: {networkInfo?.network}</p>
+        <p>
+          networkInfo.nftMarketABI.address: {networkInfo?.nftMarketABI?.address}
+        </p>
+        <p>networkInfo.nftABI.address: {networkInfo?.nftABI?.address}</p>
+        <p>
+          networkInfo.snadoFaucetABI.address:{' '}
+          {networkInfo?.snadoFaucetABI?.address}
+        </p>
+        <p>environment: {environment}</p>
+      </div>
     </footer>
   )
 }
